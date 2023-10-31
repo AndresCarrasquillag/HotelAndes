027a5aecafbbf84ced26b5a03f5fe72b005ebc91
davidzamora9aSyC <you@example.com>
2023-10-31 08:17:14 -0500

    cambios que hizo carrasquilla en clase

--- hotelandes/src/main/java/uniandes/edu/co/hotelandes/repositorio/ConsumoRepository.java
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/repositorio/ConsumoRepository.java
@@ -15,11 +15,7 @@ public interface ConsumoRepository extends JpaRepository<Consumo, Integer> {
     @Query(value = "SELECT * FROM Consumos", nativeQuery = true)
     Collection<Consumo> darConsumos();
 
-    @Query(value = "SELECT COUNT(*) as frecuencia\n" + //
-            "FROM consumos\n" + //
-            "GROUP BY servicios_id\n" + //
-            "ORDER BY frecuencia DESC\n" + //
-            "FETCH FIRST 20 ROWS ONLY", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) as frecuencia FROM Consumos GROUP BY servicios_id ORDER BY frecuencia DESC FETCH FIRST 20 ROWS ONLY", nativeQuery = true)
     Collection<Consumo> darServiciosPopulares();
 
 
