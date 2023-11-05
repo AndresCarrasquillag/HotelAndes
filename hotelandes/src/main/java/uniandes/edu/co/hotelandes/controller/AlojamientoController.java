c4164932ad13c6ad517fe88a5f8daf08392deee5
Andres Carrasquilla <Andres>
2023-11-05 16:43:11 -0500

    REQ 7 TERMINADO

--- hotelandes/src/main/java/uniandes/edu/co/hotelandes/controller/AlojamientoController.java
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/controller/AlojamientoController.java
@@ -59,5 +59,10 @@ public class AlojamientoController {
         return "redirect:/alojamientos";
     }
 
+    @GetMapping("/buenosClientesAlojamiento")
+    public String buenosClientesAlojamiento(Model model) {
+        model.addAttribute("clientes", alojamientoRepository.darBuenosClientesPorAlojamiento());
+        return "buenosClientesPorAlojamiento";
+    }
 }
  
