c1d7517dbd7b83c3486bf623658e08b0416e047e
CCr0011 <C.reyv@uniandes.edu.co>
2023-11-07 23:47:59 -0500

    proyecto2

--- hotelandes/src/main/java/uniandes/edu/co/hotelandes/Carga_de_datos/carga_datos_supermercado.java
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/Carga_de_datos/carga_datos_supermercado.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 public class carga_datos_supermercado {
     public static void main(String[] args) {
-        // Tus credenciales de conexión.
+        
         String jdbcUrl = "jdbc:oracle:thin:@fn4.oracle.virtual.uniandes.edu.co:1521/PROD";
         String usuario = "ISIS2304D07202320";
         String contraseña = "LyGscAGdEemx";
@@ -20,10 +20,10 @@ public class carga_datos_supermercado {
 
             Random random = new Random();
 
-            // Asumiendo que quieres insertar 20000 registros con nombres de supermercado aleatorios.
+            
             for (int i = 1; i <= 20000; i++) {
                 int id = i;
-                // Genera un nombre aleatorio de hasta 10 caracteres.
+                
                 String nombre = "Super" + random.nextInt(1000);
 
                 preparedStatement.setInt(1, id);
