b1dad224729e8f956789be2dfbf8a5ebab036d9e
daniel felipe torres lopez <d.torresl@uniandes.edu.co>
2023-11-07 23:51:08 -0500

    cambios servicios

--- hotelandes/src/main/java/uniandes/edu/co/hotelandes/modelo/Gimnasio.java
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/modelo/Gimnasio.java
@@ -1,39 +1,58 @@
 package uniandes.edu.co.hotelandes.modelo;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "Gimnasio")
 public class Gimnasio {
-    @EmbeddedId
-    private GimnasioPK pk;
+    @Id
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Servicio servicio;
+
+   
     private Integer num_maquinas;
 
     public Gimnasio(){
         ;
     }
 
-    public Gimnasio(Servicio id, Integer num_maquinas){
-        this.pk = new GimnasioPK(id);
+    public Gimnasio(Servicio servicio, Integer num_maquinas){
+        this.servicio = servicio;
         this.num_maquinas = num_maquinas; 
 
     }
 
-    public GimnasioPK getPk() {
-        return pk;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public Integer getNum_maquinas() {
         return num_maquinas;
     }
 
-    public void setPk(GimnasioPK pk) {
-        this.pk = pk;
-    }
-
+    
     public void setNum_maquinas(Integer num_maquinas) {
         this.num_maquinas = num_maquinas;
     } 
+
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio) {
+        this.servicio = servicio;
+    }
     
     
 }
